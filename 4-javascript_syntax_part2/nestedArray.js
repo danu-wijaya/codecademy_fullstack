da9246ef692847495858a3e80c2ffac70d159964
Kamandanu Tresna Wijaya <email@nkamandanu.id>
2021-07-08 00:12:58 +0700

    nested array

---
+++ 4-javascript_syntax_part2/nestedArray.js
@@ -0,0 +1,14 @@
+/*
+// NESTED ARRAY
+const nestedArr = [[2], [2,3]];
+
+// access element index 1 in nested aray
+console.log(nestedArr[1])
+
+// access element within nested array\
+console.log(nestedArr[1][0])
+*/
+
+const numberClusters = [[1,2], [3,4], [5,6]];
+const target = numberClusters[2][1];
+console.log(target)
